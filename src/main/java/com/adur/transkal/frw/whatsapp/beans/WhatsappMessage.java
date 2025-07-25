e45a3db97dee58150ad72010f2f0f09e714e8ffc
jluchett <jluchett@hotmail.com>
2025-07-25 11:00:33 -0500

    Agregado la clase WhatsappMessage para gestionar mensajes en la aplicación WhatsApp JSF

---
+++ src/main/java/com/adur/transkal/frw/whatsapp/beans/WhatsappMessage.java
@@ -0,0 +1,27 @@
+package com.adur.transkal.frw.whatsapp.beans;
+
+public class WhatsappMessage {
+    private String text;
+    private String sender; // "user" o "contact"
+    private String time;
+
+    public WhatsappMessage(String text, String sender, String time) {
+        this.text = text;
+        this.sender = sender;
+        this.time = time;
+    }
+
+    // Getters and Setters
+    public String getText() {
+        return text;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+}
